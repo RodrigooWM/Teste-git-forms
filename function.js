4d17a51ca2a159c01b58e5ee0238b547fd095d4a
rodrigo <rodrigoaraujo2802@gmail.com>
2022-09-18 05:07:46 -0300

    última atualização

---
+++ function.js
@@ -0,0 +1,13 @@
+function impress(dado){
+    console.log(dado)
+}
+impress(1)
+impress('Mensagem qualquer')
+impress(["augusto, antonio, mario,1,2,3,4"])
+
+/*setTimeout(function() {
+    console.log('depois de 1 segundo')
+}),5000
+
+const http = require('https')
+const API = 'https://jsonplaceholder.typicode.com/users?_limit=2'*/
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-Esse arquivo foi criado com a finalidade de testar as funcionalidades do github. Alterado.
+Esse arquivo foi criado com a finalidade de testar as funcionalidades do github. Alterado.  
